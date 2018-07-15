c4e6665f73065130a63685c37dd01488ea25f49d
leegudala <41272974+leegudala@users.noreply.github.com>
2018-07-15 17:39:11 -0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,8 +1,1 @@
- public class Hello{
-	public static void main(String args[]){
-		System.out.println("Hello World");
-		System.out.println("hiiii");
-		System.out.println("hiiii");
-		System.out.println("hiiii");
-	}
-}
+Nen Denni Kelekesa.....!!!!!
